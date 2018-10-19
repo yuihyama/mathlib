ba414ab00f1ff41257341daef0b920e26a28cf2f
yuihyama <yuihyama@gmail.com>
2018-10-19 22:14:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,10 @@
 from mathlib.add import add
 from mathlib.sub import sub
 from mathlib.mul import mul
+from mathlib.div import div
+
+if __name__ == '__main__':
+    # Write your code here...
+    #
+    # Example:
+    # print(add(1, 2))
